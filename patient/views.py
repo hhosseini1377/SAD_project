f5c9e6d2f6d4922afb42c048a2948c8a547351bf
hhosseini1377 <hhosseini@ce.sharif.edu>
2020-04-21 19:22:40 +0430

    patient improved

--- patient/views.py
+++ patient/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.http import HttpResponse
 from django.contrib.auth import login, logout
 from .models import Patient
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        if Patient.objects.filter(user=request.user).exists():
+            return redirect('patient:profile')
+    logout(request)
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
@@ -24,17 +27,23 @@ def logout_view(request):
 
 
 def profile_view(request):
+    if not request.user.is_authenticated:
+        return redirect('patient:login')
     patient = Patient.objects.get(user=request.user)
     return render(request, 'patient/profile.html', {'patient': patient})
 
 
 def disease_records_view(request):
+    if not request.user.is_authenticated:
+        return redirect('patient:login')
     patient = Patient.objects.get(user=request.user)
     records = patient.disease_records.split("-")
     return render(request, 'patient/disease_records.html', {'records': records})
 
 
 def contact_us_view(request):
+    if not request.user.is_authenticated:
+        return redirect('patient:login')
     return render(request, 'patient/contact_us.html')
 
 
